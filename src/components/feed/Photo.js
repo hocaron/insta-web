9051604326f19702e0ae005f904a489271175356
haron47 <66551410+haron47@users.noreply.github.com>
2021-07-12 22:00:57 +0900

    Photo.js(cleaning code)

---
+++ src/components/feed/Photo.js
@@ -0,0 +1,106 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { FatText } from "../shared";
+import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBookmark,
+  faComment,
+  faHeart,
+  faPaperPlane,
+} from "@fortawesome/free-regular-svg-icons";
+import Avatar from "../Avatar";
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  max-width: 615px;
+  border-radius: 4px;
+  margin-bottom: 20px;
+`;
+
+const PhotoHeader = styled.div`
+  padding: 15px 15px;
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 15px;
+`;
+
+const PhotoFile = styled.img`
+  min-width: 100%;
+`;
+
+const PhotoData = styled.div`
+  padding: 12px 15px;
+`;
+
+const PhotoActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  div {
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    font-size: 20px;
+  }
+`;
+
+const PhotoAction = styled.div`
+  margin-right: 10px;
+`;
+
+const Likes = styled(FatText)`
+  margin-top: 10px;
+  display: block;
+`;
+
+function Photo({ id, user, file, isLiked, likeNumber }) {
+  return (
+    <PhotoContainer key={id}>
+      <PhotoHeader>
+        <Avatar lg url={user.avatar}></Avatar>
+        <Username>{user.username}</Username>
+      </PhotoHeader>
+      <PhotoFile src={file} />
+      <PhotoData>
+        <PhotoActions>
+          <div>
+            <PhotoAction>
+              <FontAwesomeIcon
+                style={{ color: isLiked ? "tomato" : "inherit" }}
+                icon={isLiked ? SolidHeart : faHeart}
+              />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faComment} />
+            </PhotoAction>
+            <PhotoAction>
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </PhotoAction>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faBookmark} />
+          </div>
+        </PhotoActions>
+        <Likes>{likeNumber === 1 ? "1 like" : `${likeNumber} likes`}</Likes>
+      </PhotoData>
+    </PhotoContainer>
+  );
+}
+
+Photo.propTypes = {
+  id: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    avatar: PropTypes.string,
+    username: PropTypes.string.isRequired,
+  }),
+  file: PropTypes.string.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  likeNumber: PropTypes.number.isRequired,
+};
+export default Photo;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
-import {
-  faBookmark,
-  faComment,
-  faHeart,
-  faPaperPlane,
-} from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { symbol } from "prop-types";
 import { useHistory } from "react-router-dom";
-import styled from "styled-components";
-import { LogUserOut } from "../apollo";
-import Avatar from "../components/Avatar";
-import { FatText } from "../components/shared";
-import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
+import Photo from "../components/feed/Photo";
 
 const FEED_QUERY = gql`
   query seeFeed {
@@ -33,91 +21,13 @@ const FEED_QUERY = gql`
   }
 `;
 
-const PhotoContainer = styled.div`
-  background-color: white;
-  border: 1px solid ${(props) => props.theme.borderColor};
-  max-width: 615px;
-  border-radius: 4px;
-  margin-bottom: 20px;
-`;
-
-const PhotoHeader = styled.div`
-  padding: 15px 15px;
-  display: flex;
-  align-items: center;
-`;
-
-const Username = styled(FatText)`
-  margin-left: 15px;
-`;
-
-const PhotoFile = styled.img`
-  min-width: 100%;
-`;
-
-const PhotoData = styled.div`
-  padding: 12px 15px;
-`;
-
-const PhotoActions = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  div {
-    display: flex;
-    align-items: center;
-  }
-  svg {
-    font-size: 20px;
-  }
-`;
-
-const PhotoAction = styled.div`
-  margin-right: 10px;
-`;
-
-const Likes = styled(FatText)`
-  margin-top: 10px;
-  display: block;
-`;
-
 function Home() {
   const { data } = useQuery(FEED_QUERY);
   const history = useHistory();
   return (
     <div>
       {data?.seeFeed?.map((photo) => (
-        <PhotoContainer key={photo.id}>
-          <PhotoHeader>
-            <Avatar lg url={photo.user.avatar}></Avatar>
-            <Username>{photo.user.username}</Username>
-          </PhotoHeader>
-          <PhotoFile src={photo.file} />
-          <PhotoData>
-            <PhotoActions>
-              <div>
-                <PhotoAction>
-                  <FontAwesomeIcon
-                    style={{ color: photo.isLiked ? "tomato" : "inherit" }}
-                    icon={photo.isLiked ? SolidHeart : faHeart}
-                  />
-                </PhotoAction>
-                <PhotoAction>
-                  <FontAwesomeIcon icon={faComment} />
-                </PhotoAction>
-                <PhotoAction>
-                  <FontAwesomeIcon icon={faPaperPlane} />
-                </PhotoAction>
-              </div>
-              <div>
-                <FontAwesomeIcon icon={faBookmark} />
-              </div>
-            </PhotoActions>
-            <Likes>
-              {photo.likeNumber === 1 ? "1 like" : `${photo.likeNumber} likes`}
-            </Likes>
-          </PhotoData>
-        </PhotoContainer>
+        <Photo key={photo.id} {...photo} />
       ))}
       {/* <button onClick={() => LogUserOut(history)}>Log out now</button> */}
     </div>
