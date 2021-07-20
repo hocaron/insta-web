d92e1d68c0e0836af48a0a9749830518b59bc77a
haron47 <66551410+haron47@users.noreply.github.com>
2021-07-20 22:03:07 +0900

    Parsing Hashtags 구현완료

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -2,11 +2,13 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
 import sanitizeHtml from "sanitize-html";
+import { Link } from "react-router-dom";
+import React from "react";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
-  mark {
+  a {
     background-color: inherit;
     color: ${(props) => props.theme.blue};
     cursor: pointer;
@@ -16,25 +18,27 @@ const CommentCaption = styled.span`
   }
 `;
 
+// /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/;
+
 function Comment({ author, payload }) {
-  const cleanedPayload = sanitizeHtml(
-    payload
-      ? payload.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>")
-      : "",
-    {
-      allowedTags: ["mark"],
-    }
-  );
-  console.log("test");
-  console.log(payload?.replace(/#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/g, "<mark>$&</mark>"));
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption
-        dangerouslySetInnerHTML={{
-          __html: cleanedPayload,
-        }}
-      />
+      <CommentCaption>
+        {payload
+          ? payload.split(" ").map((word, index) =>
+              /#[ㄱ-ㅎ|ㅏ-ㅣ|가-힣|\w]+/.test(word) ? (
+                <React.Fragment key={index}>
+                  <Link key={index} to={`/hashtags/${word}`}>
+                    {word}
+                  </Link>{" "}
+                </React.Fragment>
+              ) : (
+                <React.Fragment key={index}>{word} </React.Fragment>
+              )
+            )
+          : null}
+      </CommentCaption>
     </CommentContainer>
   );
 }
