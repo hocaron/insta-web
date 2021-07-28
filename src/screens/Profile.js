b7940b87b9062f2df5e3f9fb0ab2376fb55b814d
haron47 <66551410+haron47@users.noreply.github.com>
2021-07-28 20:53:15 +0900

    Fragment

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,8 +1,36 @@
+import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import { PHOTO_FRAGMENT } from "../fragments";
+
+const SEE_PROFILE_QUERY = gql`
+  query seeProfile($username: String!) {
+    seeProfile(username: $username) {
+      id
+      firstName
+      lastNmae
+      username
+      bio
+      avatar
+      photos {
+        ...PhotoFragment
+      }
+      totalFollowing
+      totalFollowers
+      isFollowing
+      isMe
+    }
+  }
+  ${PHOTO_FRAGMENT}
+`;
 
 function Profile() {
-  const params = useParams();
-  console.log(params);
+  const { username } = useParams();
+  const { data } = useQuery(SEE_PROFILE_QUERY, {
+    variables: {
+      username,
+    },
+  });
+  console.log(data);
   return "Profile";
 }
 export default Profile;
